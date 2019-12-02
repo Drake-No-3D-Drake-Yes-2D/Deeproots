1ffe2f2dc887da827c2dcf5b18fb81afa247b704
Blake Williams <michaelbwilliams100@gmail.com>
2019-12-02 07:08:25 -0500

    ugh

--- client/src/components/PaymentModal.js
+++ client/src/components/PaymentModal.js
@@ -4,24 +4,29 @@ import './PaymentModal.css'
 
 const PaymentModal = ({ isShowing, hide, price, title}) => isShowing ? ReactDOM.createPortal(
   <React.Fragment>
-    <div className="modal-overlay"/>
-    <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className="modal">
-        <div className="modal-header">
-          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-            <span aria-hidden="true">&times;</span>
-          </button>
+
+    <div className="modal-overlay"></div>
+      <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+        <div className="modal">
+          <div className="modal-header">
+            <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div id="form-container">
+            <div id="sq-card-number"></div>
+              <div className="third" id="sq-expiration-date"></div>
+              <div className="third" id="sq-cvv"></div>
+            <div className="third" id="sq-postal-code"></div>
+            <button id="sq-creditcard" className="button-credit-card" onClick="onGetCardNonce(event)">Pay $1.00</button>
+          </div> 
         </div>
-        <div id="form-container">
-          <div id="sq-card-number"></div>
-            <div className="third" id="sq-expiration-date"></div>
-            <div className="third" id="sq-cvv"></div>
-          <div className="third" id="sq-postal-code"></div>
-          <button id="sq-creditcard" className="button-credit-card" onClick="onGetCardNonce(event)">Pay $1.00</button>
-        </div> 
       </div>
-    </div>
+    
   </React.Fragment>, document.body
 ) : null;
 
 export default PaymentModal;
+
+
+
